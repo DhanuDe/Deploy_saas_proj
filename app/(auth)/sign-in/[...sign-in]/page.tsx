ff93ccbc1d8faf12427ac31783fb8d68cc2c7874
DhanuDe <dhanukadissanayaka981220@gmail.com>
2024-06-13 15:53:50 +0530

    erroo
    checked

--- app/(auth)/sign-in/[...sign-in]/page.tsx
+++ app/(auth)/sign-in/[...sign-in]/page.tsx
@@ -1,11 +1,23 @@
-import { SignIn } from '@clerk/nextjs'
-
+import { GetStaticPaths, GetStaticProps } from 'next';
+import { SignIn } from '@clerk/nextjs';
 
 const SignInPage = () => {
-  return (
-    <SignIn/>
-  )
-}
-export function generateStaticParams() {
-  }
-export default SignInPage
+  return <SignIn />;
+};
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  // Specify the paths you want to pre-render
+  const paths = [{ params: { 'sign-in': [''] } }];
+
+  // { fallback: false } means other routes should 404
+  return { paths, fallback: false };
+};
+
+export const getStaticProps: GetStaticProps = async () => {
+  // No dynamic data fetching needed for this static page
+  return {
+    props: {}, // Will be passed to the page component as props
+  };
+};
+
+export default SignInPage;
--- app/(auth)/sign-up/[...sign-up]/page.tsx
+++ app/(auth)/sign-up/[...sign-up]/page.tsx
@@ -1,11 +1,25 @@
-import { SignUp } from '@clerk/nextjs'
-
+import { GetStaticPaths, GetStaticProps } from 'next';
+import { SignUp } from '@clerk/nextjs';
 
 const SignUpPage = () => {
-  return (
-    <SignUp/>
-  )
-}
-export function generateStaticParams() {
-}
-export default SignUpPage
+  return <SignUp />;
+};
+
+// Define static paths for dynamic routes
+export const getStaticPaths: GetStaticPaths = async () => {
+  // Specify the paths you want to pre-render
+  const paths = [{ params: { 'sign-up': [''] } }];
+
+  // { fallback: false } means other routes should 404
+  return { paths, fallback: false };
+};
+
+// Fetch data at build time
+export const getStaticProps: GetStaticProps = async () => {
+  // No dynamic data fetching needed for this static page
+  return {
+    props: {}, // Will be passed to the page component as props
+  };
+};
+
+export default SignUpPage;
