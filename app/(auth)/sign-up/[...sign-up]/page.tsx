c53c8f7bd7d4b1440064a5c3754443e7dc761aea
DhanuDe <dhanukadissanayaka981220@gmail.com>
2024-06-13 15:55:13 +0530

    remake
    remaked

--- app/(auth)/sign-up/[...sign-up]/page.tsx
+++ app/(auth)/sign-up/[...sign-up]/page.tsx
@@ -1,25 +1,10 @@
-import { GetStaticPaths, GetStaticProps } from 'next';
-import { SignUp } from '@clerk/nextjs';
+import { SignUp } from '@clerk/nextjs'
+
 
 const SignUpPage = () => {
-  return <SignUp />;
-};
+  return (
+    <SignUp/>
+  )
+}
 
-// Define static paths for dynamic routes
-export const getStaticPaths: GetStaticPaths = async () => {
-  // Specify the paths you want to pre-render
-  const paths = [{ params: { 'sign-up': [''] } }];
-
-  // { fallback: false } means other routes should 404
-  return { paths, fallback: false };
-};
-
-// Fetch data at build time
-export const getStaticProps: GetStaticProps = async () => {
-  // No dynamic data fetching needed for this static page
-  return {
-    props: {}, // Will be passed to the page component as props
-  };
-};
-
-export default SignUpPage;
+export default SignUpPage
